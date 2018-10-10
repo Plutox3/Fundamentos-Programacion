61384f99a9dc3e6c870785877774562a41465ab0
Plutox3 <43891925+Plutox3@users.noreply.github.com>
2018-10-10 12:57:41 -0600

    ejercicio en clase

---
+++ numeromayor.java
@@ -0,0 +1,24 @@
+
+public class MayorNumero {
+
+   
+    public static void main(String[] args) {
+       int a = 4;
+       int b = 8;
+       int c = 15;
+       
+       if((a>b)&&(a>c)){
+       System.out.println("El numero mayor es: "+ a);
+       }
+       
+        if((b>a)&&(b>c)){
+       System.out.println("El numero mayor es: "+ b);
+       }
+        
+        if((c>a)&&(c>b)){
+       System.out.println("El numero mayor es: "+ c);
+       } 
+       
+    }
+    
+}
