08b38328b101d187662e61bfe0819eedb760212f
Plutox3 <43891925+Plutox3@users.noreply.github.com>
2018-11-13 16:34:30 -0600

    Update and rename Felizmain17161056.java to Feliz2main17161056.java

---
+++ Feliz2main17161056.java
@@ -0,0 +1,46 @@
+public class Feliz2main17161056 {
+  
+    public static void main(String[] args) 
+{ 
+    int n = Integer.parseInt(args[0]); 
+    if (sinumerofeliz(n)) 
+        System.out.println(1); 
+    else
+        System.out.println(0); 
+} 
+
+static int nuevasumacuadrados(int n) 
+{ 
+    int sumacuadrados = 0; 
+    while (n!= 0) 
+    { 
+        sumacuadrados += (n % 10) * (n % 10); 
+        n /= 10; 
+    } 
+    return sumacuadrados; 
+} 
+    
+static boolean sinumerofeliz(int n) 
+{ 
+    int d, s; 
+   
+ 
+    d = s = n; 
+    do
+    { 
+
+        d = nuevasumacuadrados(d); 
+   
+    
+        s = nuevasumacuadrados(nuevasumacuadrados(s)); 
+   
+    } 
+    while (d != s); 
+   
+ 
+    return (d == 1); 
+} 
+   
+ 
+
+} 
