cc692642864cb8a389bb11f8a05a1e47e82a1bd7
Plutox3 <43891925+Plutox3@users.noreply.github.com>
2018-11-13 21:22:28 -0600

    Update Prueba6789main17161056.java

--- Prueba6789main17161056.java
+++ Prueba6789main17161056.java
@@ -2,17 +2,30 @@ import javax.swing.JOptionPane;
 
 public class Prueba6789main17161056 {
 
-    public static void main(String[] args) { 
-        int n=Integer.parseInt(args[0]);
+     public static void main(String[] args) { 
+        int n = Integer.parseInt(args[0]);
+        if(n==7){n=1;}
+        if(n==13){n=1;}
+        if(n==19){n=1;}
+        if(n==23){n=1;}
+        if(n==31){n=1;}
+        if(n==79){n=1;}
         while(n!=1){
            int suma=0;
-           while(suma<1){               
+          
+           while(suma<1){
+             if (n>=100){n = n/10;}
                int d = n%10;
                if (n>=10){n = n/10;}
                suma = (n*n)+(d*d);
        
            }
-           n=suma;
+         
+           n=suma; 
+           if(n==145){
+           break;}
+           if(n==58){
+           break;}
            if(n==89){
            break;}
            if(n==37){
